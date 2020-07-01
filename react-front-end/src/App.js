51dd1829ffdc25eb52532d13e9c1901be14e08b7
Junsung Park <zeipar@gmail.com>
2020-07-01 11:57:33 -0700

    Linted and commented out <body> tag under <div> tag.

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,53 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import MainPage from './pages/home';
-import Login from './pages/login'
-import SignIn from './pages/signIn'
-import Profile from './pages/profile'
-import Platforms from './pages/platforms'
-import Games from './pages/addGames'
-import Search from './pages/search'
-import Game from './pages/game'
+import React, { useState } from "react";
+import "./App.css";
+import MainPage from "./pages/home";
+import Login from "./pages/login";
+import SignIn from "./pages/signIn";
+import Profile from "./pages/profile";
+import Platforms from "./pages/platforms";
+import Games from "./pages/addGames";
+import Search from "./pages/search";
+import Game from "./pages/game";
 
-import AppBar from './components/navbar'
+import AppBar from "./components/navbar";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export default function App (){
+export default function App() {
   const [userState, setUserState] = useState({
-    name: '',
-    lastName:'',
-    userId:0,
-    login:false,
-    mail:'',
+    name: "",
+    lastName: "",
+    userId: 0,
+    login: false,
+    mail: "",
   });
 
   const [gameState, setGameState] = useState({
-    name: '',
+    name: "",
   });
 
-    return (
-      <Router>
+  return (
+    <Router>
       <AppBar userState={userState} setUserState={setUserState} />
-      <body >
-        <Switch>
+      {/* <body > */}
+      <Switch>
         <Route exact path="/" component={MainPage} />
-        <Route exact path="/login"> <Login userState={userState} setUserState={setUserState}/></Route>
-        <Route exact path="/signIn"> <SignIn userState={userState} setUserState={setUserState}/></Route>
-        <Route exact path="/profile"> <Profile userState={userState} setUserState={setUserState}/></Route>
-        <Route exact path="/platforms"> <Platforms userState={userState} setUserState={setUserState}/></Route>
-        <Route exact path="/games"> <Games userState={userState} setUserState={setUserState}/></Route>
-        <Route exact path="/search"> <Search userState={userState} setUserState={setUserState} gameState={gameState} setGameState={setGameState}/></Route>
-        <Route exact path="/game"> <Game userState={userState} setUserState={setUserState} gameState={gameState} setGameState={setGameState}/></Route>
-
-
+        <Route exact path="/login">
+          <Login userState={userState} setUserState={setUserState} />
+        </Route>
+        <Route exact path="/signIn">
+          <SignIn userState={userState} setUserState={setUserState} />
+        </Route>
+        <Route exact path="/profile">
+          <Profile userState={userState} setUserState={setUserState} />
+        </Route>
+        <Route exact path="/platforms">
+          <Platforms userState={userState} setUserState={setUserState} />
+        </Route>
+        <Route exact path="/games">
+          <Games userState={userState} setUserState={setUserState} />
+        </Route>
+        <Route exact path="/search">
+          <Search
+            userState={userState}
+            setUserState={setUserState}
+            gameState={gameState}
+            setGameState={setGameState}
+          />
+        </Route>
+        <Route exact path="/game">
+          <Game
+            userState={userState}
+            setUserState={setUserState}
+            gameState={gameState}
+            setGameState={setGameState}
+          />
+        </Route>
       </Switch>
-      </body>
+      {/* </body> */}
     </Router>
   );
 }
