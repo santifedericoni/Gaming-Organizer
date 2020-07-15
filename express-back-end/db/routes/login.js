b890256a4c69e815982b6937cfb1e0242acbc8c9
Junsung Park <zeipar@gmail.com>
2020-07-15 01:26:14 -0700

    Added db query logic to passport.js. Preparing to serializeUser into session data after correct db fetch.

--- express-back-end/db/routes/login.js
+++ express-back-end/db/routes/login.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-
 module.exports = db => {
   const passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy;
+    LocalStrategy = require("passport-local").Strategy;
+
+  router.use(passport.initialize());
+  router.use(passport.session());
+
+  passport.serializeUser((user, done) => {
+    // done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    // User.findById(id, function(err, user) {
+    //   done(err, user);
+    // });
+  });
+
   passport.use(
     new LocalStrategy(
       {
@@ -12,35 +25,34 @@ module.exports = db => {
         passwordField: "password",
       },
       (username, password, done) => {
-        console.log("Local", username, password);
-        // User.findOne({ username: username }, (err, user) => {
-        //   if (err) {
-        //     return done(err);
-        //   }
-        //   if (!user) {
-        //     return done(null, false);
-        //   }
-        //   if (!user.verifyPassword(password)) {
-        //     return done(null, false);
-        //   }
-        //   return done(null, user);
-        // });
+        const values = [username, password];
+        let query = `
+            SELECT * FROM users WHERE email = $1 and password = $2;
+            `;
+
+        db.query(query, values)
+          .then(data => {
+            if (!data.rows.length) {
+              return done(null, false, { msg: "Invalid email or password" });
+            }
+            console.log(data.rows[0]);
+            return done(null, data.rows[0]);
+          })
+          .catch(err => {
+            return done(err);
+          });
       }
     )
   );
 
-  router.post(
-    "/login_process",
-    passport.authenticate("local"),
-    (req, res) => {
-      const path = {};
-      if (req.user) {
-        path.redirect = "/";
-      } else {
-        path.redirect = "/login";
-      }
+  router.post("/login_process", passport.authenticate("local"), (req, res) => {
+    const path = {};
+    if (req.user) {
+      path.redirect = "/";
+    } else {
+      path.redirect = "/login";
     }
-  );
+  });
 
   // router.post('/login', (req, res) => {
   //   const email = req.body.form.email;
