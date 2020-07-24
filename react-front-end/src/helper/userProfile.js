243658d94878f84bf61733ef18b9213b0ef29ac2
Junsung Park <zeipar@gmail.com>
2020-07-23 18:07:52 -0700

    Removed files no longer in use.

--- react-front-end/src/helper/userProfile.js
+++
@@ -1,9 +0,0 @@
-import react, { useState } from "react";
-
-export default function userProfile() {
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
- 
-}
